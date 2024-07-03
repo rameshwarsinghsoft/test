ae2baa5e8bb5581dfe9cb71d873de606d99d8dca
rameshwarsinghsoft <rameshwar02@singhsoft.com>
2024-07-03 10:49:06 +0530

    add array folder in which filter method explain

---
+++ Array/filter.js
@@ -0,0 +1,9 @@
+let students = [
+    { name: "Rameshwar", age: 29, city: "Jaipur" },
+    { name: "Piyush", age: 9, city: "Agar Malwa" },
+    { name: "Jyoti", age: 28, city: "Tanodiya" }
+]
+
+students.filter((value)=>{
+    console.log(value)
+})
